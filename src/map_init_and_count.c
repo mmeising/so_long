9d3733d312d099041c9403f60d58aff88e0ca0b7
Max Meisinger <mmeising@student.42heilbronn.de>
2021-11-18 22:11:12 +0100

    saving player position in struct at first P of map, swapping every 'P' in 2D array to a '0'

--- src/map_init_and_count.c
+++ src/map_init_and_count.c
@@ -6,7 +6,7 @@
 /*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:24:56 by mmeising          #+#    #+#             */
-/*   Updated: 2021/11/18 21:57:50 by mmeising         ###   ########.fr       */
+/*   Updated: 2021/11/18 22:11:07 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	is_pec(t_map *map, size_t x, size_t y)
 {
 	if (ft_in_set(map->map[y][x], "P"))
 	{
-		if (map->count_p > 0)
-			map->map[y][x] = '0';
-		else if (map->count_p == 0)
+		if (map->count_p == 0)
 			map->p_pos = set_coords(x, y);
+		map->map[y][x] = '0';
 		map->count_p++;
 	}
 	else if (ft_in_set(map->map[y][x], "E"))
